33dc9e19d429b0afd9cc605efa127aaadd6f2e25
Ilya Konyakhin <mathers_beats@mail.ru>
2022-08-20 14:53:41 +0300

    add: decorator

---
+++ decorator.php
@@ -0,0 +1,56 @@
+<?php
+
+interface INotifier
+{
+    public function notifier(string $content): void;
+}
+
+class Notify implements INotifier
+{
+    private $text;
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+    public function sendMessage():string
+    {
+        return message($this->text);
+    }
+}
+
+abstract class Decorator implements INotifier
+{
+    protected $content = null;
+    public function __construct(INotifier $content)
+    {
+        $this->content = $content;
+    }
+}
+class Sms extends Decorator
+{
+    public function sendMessage(): string
+    {
+        return message($this->content->sendMessage);
+    }
+}
+class Email extends Decorator
+{
+    public function sendMessage(): string
+    {
+        return message($this->content->sendMessage);
+    }
+}
+class CN extends Decorator
+{
+    public function sendMessage(): string
+    {
+        return message($this->content->sendMessage);
+    }
+}
+
+// test example
+function testDecorator(string $message)
+{
+    $notification = new Sms(new Email(new Notify($message)));
+    $notification->message();
+}
